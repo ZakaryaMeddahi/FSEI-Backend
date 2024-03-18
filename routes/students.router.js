ce8c09cfc473016a72f3397a529139f034b2b387
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-18 14:26:57 +0100

    Add deleteStudent

--- routes/students.router.js
+++ routes/students.router.js
@@ -4,9 +4,10 @@ const {
   getAllStudents,
   createStudent,
   updateStudent,
+  deleteStudent
 } = require('../controllers/students.controller');
 
 router.route('/').get(getAllStudents).post(createStudent);
-router.route('/:id').get(getStudent).patch(updateStudent);
+router.route('/:id').get(getStudent).patch(updateStudent).delete(deleteStudent);
 
 module.exports = router;
