799ddc8de92844f94117d8f1dbd6d14ac335572c
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-18 14:28:56 +0100

    Update routes middlewares

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require('./db/connect');
 
 const app = express();
 
 // Import middlewares
+const authMiddleware = require('./middlewares/auth.middleware');
 const errorHandler = require('./middlewares/error.middleware');
 
 // Import routers
 const authRouter = require('./routes/auth.router');
-const connectDB = require('./db/connect');
+const accountRouter = require('./routes/account.router');
+const studentsRouter = require('./routes/students.router');
+const projectsRouter = require('./routes/projects.router');
+const blogsRouter = require('./routes/blogs.router');
+const fieldsRouter = require('./routes/fields.router');
+const modulesRouter = require('./routes/modules.router');
+const announcementRouter = require('./routes/announcements.router');
+const { NotFoundError } = require('./utils/errors');
+// const resourcesRouter = require('./routes/resources.router');
+// const planningsRoute = require('./routes/plannings.router');
 
 // Constants
 const PORT = process.env.PORT || 8080;
@@ -18,10 +29,21 @@ const URI = process.env.MONGO_URI;
 app.use(express.json());
 
 // Routes
-app.use('api/v1/auth', authRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/account', authMiddleware, accountRouter);
+app.use('/api/v1/students', studentsRouter);
+app.use('/api/v1/projects', authMiddleware, projectsRouter);
+app.use('/api/v1/blogs', authMiddleware, blogsRouter);
+app.use('/api/v1/fields', fieldsRouter);
+app.use('/api/v1/modules', modulesRouter);
+// app.use('/api/v1/fields/:fieldId/modules', modulesRouter);
+// app.use('/api/v1/fields/:fieldId/plannings', planningsRoute);
+// app.use('/api/v1/modules/:moduleId/resources', resourcesRouter);
+app.use('/api/v1/announcements', announcementRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('*', (req, res, next) => {
+  const error = new NotFoundError('Route not found');
+  next(error);
 });
 
 app.use(errorHandler);
