669e38ed60d128da727726a5a34d857173b86790
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-03-18 19:59:13 +0100

    Add admins router

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = express();
 
 // Import middlewares
 const authMiddleware = require('./middlewares/auth.middleware');
+const adminMiddleware = require('./middlewares/admin.middleware');
 const errorHandler = require('./middlewares/error.middleware');
 
 // Import routers
 const authRouter = require('./routes/auth.router');
 const accountRouter = require('./routes/account.router');
+const adminsRouter = require('./routes/admins.router');
 const studentsRouter = require('./routes/students.router');
 const projectsRouter = require('./routes/projects.router');
 const blogsRouter = require('./routes/blogs.router');
@@ -31,15 +33,16 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/account', authMiddleware, accountRouter);
-app.use('/api/v1/students', studentsRouter);
+app.use('/api/v1/admins', authMiddleware, adminMiddleware, adminsRouter);
+app.use('/api/v1/students', authMiddleware, adminMiddleware, studentsRouter);
 app.use('/api/v1/projects', authMiddleware, projectsRouter);
 app.use('/api/v1/blogs', authMiddleware, blogsRouter);
-app.use('/api/v1/fields', fieldsRouter);
-app.use('/api/v1/modules', modulesRouter);
+app.use('/api/v1/fields', authMiddleware, fieldsRouter);
+app.use('/api/v1/modules', authMiddleware, modulesRouter);
 // app.use('/api/v1/fields/:fieldId/modules', modulesRouter);
 // app.use('/api/v1/fields/:fieldId/plannings', planningsRoute);
 // app.use('/api/v1/modules/:moduleId/resources', resourcesRouter);
-app.use('/api/v1/announcements', announcementRouter);
+app.use('/api/v1/announcements', authMiddleware, announcementRouter);
 
 app.get('*', (req, res, next) => {
   const error = new NotFoundError('Route not found');
